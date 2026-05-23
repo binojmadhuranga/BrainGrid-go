7602106de73e43fa629f16fa05b7f81b20571d0b
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-05-23 22:26:42 +0530

    Add AdminMiddleware for role-based access control; update admin routes to use new middleware

--- routes/admin_routes.go
+++ routes/admin_routes.go
@@ -1,35 +1,35 @@
 package routes
 
 import (
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 
-    "your-project/controllers"
-    "your-project/middlewares"
+	"github.com/binojmadhuranga/BrainGrid-go/controllers"
+	"github.com/binojmadhuranga/BrainGrid-go/middleware"
 )
 
 func AdminRoutes(r *gin.RouterGroup) {
 
-    admin := r.Group("/admin")
+	admin := r.Group("/admin")
 
-    admin.Use(middlewares.JWTMiddleware())
-    admin.Use(middlewares.AdminMiddleware())
+	admin.Use(middleware.JWTAuthMiddleware())
+	admin.Use(middleware.AdminMiddleware())
 
-    {
-        admin.GET("/users", controllers.GetUsers)
+	{
+		admin.GET("/users", controllers.GetUsers)
 
-        admin.PUT(
-            "/users/:id/role",
-            controllers.UpdateUserRole,
-        )
+		admin.PUT(
+			"/users/:id/role",
+			controllers.UpdateUserRole,
+		)
 
-        admin.PUT(
-            "/users/:id/status",
-            controllers.UpdateUserStatus,
-        )
+		admin.PUT(
+			"/users/:id/status",
+			controllers.UpdateUserStatus,
+		)
 
-        admin.DELETE(
-            "/users/:id",
-            controllers.DeleteUser,
-        )
-    }
+		admin.DELETE(
+			"/users/:id",
+			controllers.DeleteUser,
+		)
+	}
 }

4d312c7369644924ed12ad4250916d023c0e8996
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-05-26 21:05:12 +0530

    Enhance JWT middleware with improved error handling and add admin routes for user management

--- utils/jwt.go
+++ utils/jwt.go
@@ -3,7 +3,7 @@ package utils
 import (
 	"os"
 	"time"
-    
+
 	"github.com/binojmadhuranga/BrainGrid-go/models"
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -25,4 +25,26 @@ func GenerateToken(user models.User) (string, error) {
 	)
 
 	return token.SignedString(jwtKey)
+}
+
+func ValidateToken(tokenString string) (jwt.MapClaims, error) {
+
+	token, err := jwt.Parse(
+		tokenString,
+		func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+
+	if !ok || !token.Valid {
+		return nil, err
+	}
+
+	return claims, nil
 }
